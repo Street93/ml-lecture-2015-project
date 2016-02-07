a4d0f005842e99105e4e153d883999b128013f1c
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2016-02-08 00:52:54 +0100

    Save raw data instead of proccessed one, refactor

---
+++ sanitize.py
@@ -0,0 +1,100 @@
+def long_enough(paragraph):
+    return len(paragraph) >= 10
+
+def good_start(paragraph):
+    return paragraph[0].isalpha() and paragraph[0].isupper()
+
+def good_end(paragraph):
+    good_ends = ['.', '?', '!', '."', '?"', '!"']
+
+    return any((paragraph.endswith(end) for end in good_ends))
+
+def good_characters(paragraph):
+    good_chars = 'abcdefghijklmnopqrstuvwxyz' \
+               + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' \
+               + 'äöüÄÖÜß' \
+               + '0123456789' \
+               + '.!?:,;"-()' \
+               + ' '
+
+    return all((c in good_chars for c in paragraph))
+
+def no_caps(paragraph):
+    i = 0
+    while i <= len(paragraph) - 2:
+        s = paragraph[i : i + 2]
+        if s.isalpha() and s.isupper():
+            return False
+        i += 1
+
+    return True
+
+def split_at(string, predicate):
+    substr_start = 0
+    i = 0
+    while i != len(string):
+        if(predicate(string[i])):
+            yield string[substr_start : i]
+            substr_start = i + 1
+        i += 1 
+
+    yield string[substr_start : ]
+
+def no_short_sentences(paragraph):
+    sentence_end_chars = ['.', '!', '?', ':']
+    sentences = list(split_at(paragraph, lambda c: c in sentence_end_chars))[ : -1]
+    # print(sentences)
+
+    for sentence in sentences:
+        words = sentence.split()
+        # print(words)
+        if len(words) < 2:
+            return False
+
+    return True
+
+
+def good_paragraph(paragraph):
+    predicates = [ long_enough \
+                 , good_start \
+                 , good_end \
+                 , good_characters \
+                 # , no_caps \
+                 , no_short_sentences ]
+
+    return all((predicate(paragraph) for predicate in predicates))
+
+def merge_and_filter_paragraphs(paragraphs):
+    result = []
+
+    i = 0
+    while i != len(paragraphs):
+        good_paragraphs = []
+        while i != len(paragraphs) and good_paragraph(paragraphs[i]):
+            good_paragraphs.append(paragraphs[i])
+            i += 1
+
+        if good_paragraphs != []:
+            result.append(' '.join(good_paragraphs))
+
+        bad_paragraphs = []
+        while i != len(paragraphs) and not good_paragraph(paragraphs[i]):
+            bad_paragraphs.append(paragraphs[i])
+            i += 1
+
+        if bad_paragraphs != [] and \
+                not good_end(bad_paragraphs[-1]) and \
+                i != len(paragraphs):
+            i += 1
+
+    return result
+
+def fix_whitespace(paragraph):
+    paragraph = paragraph.replace('\n', ' ')
+
+    words = paragraph.split(' ')
+    return ' '.join(filter(lambda str: str, words))
+
+def sanitize_article(article_paragraphs):
+    fixed_paragraphs = [fix_whitespace(paragraph) for paragraph in article_paragraphs]
+    return merge_and_filter_paragraphs(fixed_paragraphs)
---
+++ spiegel.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+from lxml import html
+import requests
+from collections import namedtuple
+from itertools import chain
+from time import sleep
+from sys import stderr
+from pathlib import Path
+import tarfile
+from io import BytesIO
+
+SpiegelIssue = namedtuple('SpiegelIssue', 'year week')
+class SpiegelIssue(namedtuple('SpiegelIssue', 'year week')):
+    def __str__(self):
+        return 'spiegel-{}-{:02}'.format(self.year, self.week)
+
+class IssueDoesNotExist(Exception):
+    pass
+
+def load_index(spiegel_issue):
+    (year, week) = spiegel_issue
+    indexUrl = 'http://www.spiegel.de/spiegel/print/index-{}-{}.html'.format(year, week)
+    page = requests.get(indexUrl)
+    if page.status_code != requests.codes.ok:
+        raise IssueDoesNotExist()
+    doc = html.fromstring(page.content)
+
+    linkElements = doc.cssselect('.spiegel-contents > ul a')
+
+    links = []
+    for linkElement in linkElements:
+        links.append('https://www.spiegel.de' + linkElement.get('href'))
+
+    return links
+
+def load_raw_articles(issue, topath='.'):
+    article_urls = load_index(issue)
+
+    archive_path = Path(topath) / (str(issue) + '.tar.gz')
+    with tarfile.open(str(archive_path), mode='x:gz') as tar:
+        for url in article_urls:
+            response = requests.get(url)
+            if response.ok:
+                file_name = url.split('/')[-1]
+                tarinfo = tarfile.TarInfo(file_name)
+                tarinfo.size = len(response.content)
+                tar.addfile(tarinfo, BytesIO(response.content))
+
+def scrape_paragraphs(page_source_bytes):
+    doc = html.fromstring(page_source_bytes)
+
+    paragraphElements = doc.cssselect('.artikel > p')
+
+    paragraphs = []
+    for paragraphElement in paragraphElements:
+        if paragraphElement.text:
+            paragraphs.append(paragraphElement.text)
+
+    return paragraphs
+
+def iter_articles(issue_archive_path):
+    with tarfile.open(issue_archive_path) as tar:
+        for member in tar:
+            f = tar.extractfile(member)
+            yield scrape_paragraphs(f.read())
+
+def iter_issues_articles(issues=None, data_path='data/spiegel'):
+    def iter_paths():
+        if issues == None:
+            for path in Path(data_path).iterdir():
+                yield path
+        else:
+            for issue in issues:
+                yield Path(data_path) / (str(issue) + '.tar.gz')
+    
+    for issue_path in iter_paths():
+        for article in iter_articles(issue_path):
+            yield article
+
+
+def main():
+    issues = (SpiegelIssue(year, week) for year in range(1990, 2010)
+                                       for week in range(1, 53))
+    for issue in issues:
+        try:
+            load_raw_articles(issue)
+        except IssueDoesNotExist:
+            pass
+        except requests.exceptions.ConnectionError as exc:
+            print('Failed to download issue {}-{:02}'.format(year, week), file=stderr)
+            print(exc, file=stderr)
+
+
+if __name__ == '__main__':
+    main()
